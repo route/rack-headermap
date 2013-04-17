a226ed58f661ecc338b09975082a6b71d548528d
Dmitry Vorotilin <d.vorotilin@gmail.com>
2013-04-17 20:01:39 +0400

    Fixed empty HTTP_X_PATHINFO

--- lib/rack/headermap.rb
+++ lib/rack/headermap.rb
@@ -7,20 +7,14 @@ module Rack
     def call(env)
       path = env['PATH_INFO']
       script_name = env['SCRIPT_NAME']
-      if needed_x_headers_set?(env)
-        env['PATH_INFO'] = env['HTTP_X_PATHINFO']
+      if env['HTTP_X_SCRIPTNAME']
+        env['PATH_INFO'] = env['HTTP_X_PATHINFO'] || '/'
         env['SCRIPT_NAME'] = env['HTTP_X_SCRIPTNAME']
       end
       @app.call(env)
     ensure
       env['PATH_INFO'] = path
       env['SCRIPT_NAME'] = script_name
-    end
-
-    private
-
-    def needed_x_headers_set?(env)
-      env['HTTP_X_PATHINFO'] && env['HTTP_X_SCRIPTNAME']
     end
   end
 end
